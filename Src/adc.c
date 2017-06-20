bbd7d3c66bd313f41a1a4848d51428259d1bf5b6
Williamzhangtw <525448585@qq.com>
2017-06-21 03:11:43 +0800

    05
    add temperature and voltage sensor of mcu

--- Src/adc.c
+++ Src/adc.c
@@ -73,7 +73,7 @@ void MX_ADC1_Init(void)
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 2;
+  hadc1.Init.NbrOfConversion = 4;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
@@ -93,6 +93,24 @@ void MX_ADC1_Init(void)
     */
   sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = 2;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure Regular Channel 
+    */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Rank = 3;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure Regular Channel 
+    */
+  sConfig.Channel = ADC_CHANNEL_VREFINT;
+  sConfig.Rank = 4;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
@@ -128,7 +146,7 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
     hdma_adc1.Init.MemInc = DMA_MINC_ENABLE;
     hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
     hdma_adc1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
-    hdma_adc1.Init.Mode = DMA_NORMAL;
+    hdma_adc1.Init.Mode = DMA_CIRCULAR;
     hdma_adc1.Init.Priority = DMA_PRIORITY_LOW;
     if (HAL_DMA_Init(&hdma_adc1) != HAL_OK)
     {
--- Src/freertos.c
+++ Src/freertos.c
@@ -65,7 +65,7 @@ osThreadId sensor1TaskHandle;
 
 /* USER CODE BEGIN Variables */
 uint8_t  cPrint[1024];
-volatile uint16_t uiADC[3];
+volatile uint16_t uiADC[10];
 /* USER CODE END Variables */
 
 /* Function prototypes -------------------------------------------------------*/
@@ -79,7 +79,61 @@ void vSensor1Task(void const * argument);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /* USER CODE BEGIN FunctionPrototypes */
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define VDD_APPLI                      ((uint32_t) 3300)   /* Value of analog voltage supply Vdda (unit: mV) */
+#define RANGE_12BITS                   ((uint32_t) 4095)   /* Max value with a full range of 12 bits */
+#define USERBUTTON_CLICK_COUNT_MAX     ((uint32_t)    4)   /* Maximum value of variable "UserButtonClickCount" */
 
+#define ADCCONVERTEDVALUES_BUFFER_SIZE ((uint32_t)    3)   /* Size of array containing ADC converted values: set to ADC sequencer number of ranks converted, to have a rank in each address */
+
+/* Internal temperature sensor: constants data used for indicative values in  */
+/* this example. Refer to device datasheet for min/typ/max values.            */
+/* For more accurate values, device should be calibrated on offset and slope  */
+/* for application temperature range.                                         */
+#define INTERNAL_TEMPSENSOR_V25        ((int32_t)1430)         /* Internal temperature sensor, parameter V25 (unit: mV). Refer to device datasheet for min/typ/max values. */
+#define INTERNAL_TEMPSENSOR_AVGSLOPE   ((int32_t)4300)         /* Internal temperature sensor, parameter Avg_Slope (unit: uV/DegCelsius). Refer to device datasheet for min/typ/max values. */                                                               /* This calibration parameter is intended to calculate the actual VDDA from Vrefint ADC measurement. */
+
+/* Private macro -------------------------------------------------------------*/
+
+/**
+  * @brief  Computation of temperature (unit: degree Celsius) from the internal
+  *         temperature sensor measurement by ADC.
+  *         Computation is using temperature sensor standard parameters (refer
+  *         to device datasheet).
+  *         Computation formula:
+  *         Temperature = (VTS - V25)/Avg_Slope + 25
+  *         with VTS = temperature sensor voltage
+  *              Avg_Slope = temperature sensor slope (unit: uV/DegCelsius)
+  *              V25 = temperature sensor @25degC and Vdda 3.3V (unit: mV)
+  *         Calculation validity conditioned to settings: 
+  *          - ADC resolution 12 bits (need to scale value if using a different 
+  *            resolution).
+  *          - Power supply of analog voltage Vdda 3.3V (need to scale value 
+  *            if using a different analog voltage supply value).
+  * @param TS_ADC_DATA: Temperature sensor digital value measured by ADC
+  * @retval None
+  */
+#define COMPUTATION_TEMPERATURE_STD_PARAMS(TS_ADC_DATA)                        \
+  ((((int32_t)(INTERNAL_TEMPSENSOR_V25 - (((TS_ADC_DATA) * VDD_APPLI) / RANGE_12BITS)   \
+     ) * 1000                                                                  \
+    ) / INTERNAL_TEMPSENSOR_AVGSLOPE                                           \
+   ) + 25                                                                      \
+  )
+
+/**
+  * @brief  Computation of voltage (unit: mV) from ADC measurement digital
+  *         value on range 12 bits.
+  *         Calculation validity conditioned to settings: 
+  *          - ADC resolution 12 bits (need to scale value if using a different 
+  *            resolution).
+  *          - Power supply of analog voltage Vdda 3.3V (need to scale value 
+  *            if using a different analog voltage supply value).
+  * @param ADC_DATA: Digital value measured by ADC
+  * @retval None
+  */
+#define COMPUTATION_DIGITAL_12BITS_TO_VOLTAGE(ADC_DATA)                        \
+  ( (ADC_DATA) * VDD_APPLI / RANGE_12BITS)
 /* USER CODE END FunctionPrototypes */
 
 /* Hook prototypes */
@@ -197,9 +251,9 @@ void vPrintfTask(void const * argument)
   /* Infinite loop */
   for(;;)
   {
-   for(uint8_t i=0;i<3;i++)
+   for(uint8_t i=0;i<5;i++)
     {
-      printf("adc :%d \n",uiADC[i]);
+      printf("adc%d :%d \n",i,uiADC[i]);
     }
     osDelay(500);
   }
@@ -214,7 +268,7 @@ void vSensor1Task(void const * argument)
      /* Start ADC conversion on regular group with transfer by DMA */
 if (HAL_ADC_Start_DMA(&hadc1,
                         (uint32_t *)uiADC,
-                        2
+                        4
                        ) != HAL_OK)
   {
     /* Start Error */
